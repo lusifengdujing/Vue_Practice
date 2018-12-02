23c4ffc60df930ff18cec1b129e9b17d678baa95
lusifeng <lsf493362328@qq.com>
2018-12-02 11:48:06 +0800

    TodoItem.vue 增加把鼠标移到块内显示灰色

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,13 @@
 <template>
-    <li>
+    <!-- onmousueenter;onmouseleave;onmouseover;onmouseout -->
+    <li @mouseenter="handleEnter(true)" @mouseleave="handleEnter(false)" :style="{background: bgColor}" >
         <label>
             <input type="checkbox" v-model="todo.complete">
             <span>{{todo.title}}</span>
         </label>
-        <button class="btn btn-danger">删除</button>
+        <span><button>shanchu</button></span>
     </li>
+  
 </template>
 
 <script>
@@ -14,6 +16,25 @@
           todo:Object,
           index:Number
 
+      },
+      data(){
+          return{
+              bgColor:'white',//默认的背景颜色
+              isShow:false//按钮默认是否显示
+          }
+      },
+      methods:{
+          handleEnter(isEnter){
+              console.log(isEnter);
+              if(isEnter){
+                  this.bgColor="#aaaaaa";
+                  this.isShow=true;
+              }else{
+                  this.bgColor="white";
+                  this.isShow=false;
+              }
+
+          }
       }
   }
 </script>
